1d914be64c873abbeecfc2350e016e120add0989
= <courbon.marcantoine@gmail.com>
2025-06-27 11:12:04 +0200

    adding color customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
+import colors from 'tailwindcss/colors'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          50: colors.green[50],
+          100: colors.green[100],
+          200: colors.green[200],
+          300: colors.green[300],
+          400: colors.green[400],
+          500: colors.green[500],
+          600: colors.green[600], 
+          700: colors.green[700],
+          800: colors.green[800],
+        }
+      }
+    },
   },
   plugins: [],
 }
-
